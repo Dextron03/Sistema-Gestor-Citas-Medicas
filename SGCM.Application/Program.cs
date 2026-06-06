4591a6bac275d40028119c0915452f13f4daf0bd
BrailyRS <78430276+Dextron03@users.noreply.github.com>
2026-06-06 14:23:28 -0400

    Primer commit

---
+++ SGCM.Application/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SGCM.Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ SGCQM.DATA/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SGCQM.DATA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
